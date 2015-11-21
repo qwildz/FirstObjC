e5972a2666d62cbbf599d8388a589183bef5ef97
Muhammad Resna Rizki Pratama <qwildz@muhammads-mac.local>
2015-11-22 01:49:10 +0900

    Initial Commit

---
+++ First/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  First
+//
+//  Created by Muhammad Resna Rizki Pratama on 11/22/15.
+//  Copyright © 2015 Muhammad Resna Rizki Pratama. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
